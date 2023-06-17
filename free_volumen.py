a1f9faa9088df55d95236a7d5c998191984c5d33
Juan Jiménez <98825720+jjimenezgar@users.noreply.github.com>
2023-06-17 16:45:54 -0300

    Update free_volumen.py

--- free_volumen.py
+++ free_volumen.py
@@ -4,68 +4,68 @@ from ovito.modifiers import ConstructSurfaceModifier, SelectTypeModifier, Delete
 
 def calculate_free_volume(input_file, lz):
     """
-    Calcula el volumen libre de un sistema molecular a partir de un archivo de entrada en formato XYZ.
+    Calculates the free volume of a molecular system from an input file in XYZ format.
 
-    Parámetros:
-    - input_file (str): Ruta al archivo de entrada en formato XYZ que contiene la estructura molecular.
-    - lz (float): Valor de la coordenada z de la celda en angstroms. Se utiliza para modificar la celda del sistema.
+    Parameters:
+    - input_file (str): Path to the input file in XYZ format containing the molecular structure.
+    - lz (float): Value of the z coordinate of the cell in angstroms. It is used to modify the system's cell.
 
-    Retorna:
-    - free_volume (float): Volumen libre del sistema en metros cúbicos.
+    Returns:
+    - free_volume (float): Free volume of the system in cubic meters.
 
-    Requiere la biblioteca OVITO: https://www.ovito.org/
+    Requires the OVITO library: https://www.ovito.org/
 
-    La función realiza los siguientes pasos:
-    1. Importa el archivo XYZ especificado.
-    2. Modifica los vectores de celda del sistema para ajustar el valor de la coordenada z a 'lz'.
-    3. Aplica transformaciones a la celda utilizando el modificador AffineTransformationModifier de OVITO.
-    4. Selecciona las partículas del tipo 'Ox' utilizando el modificador SelectTypeModifier.
-    5. Elimina las partículas seleccionadas utilizando el modificador DeleteSelectedModifier.
-    6. Construye una superficie basada en el método AlphaShape utilizando el modificador ConstructSurfaceModifier.
-    7. Calcula el volumen libre restando el volumen de la celda vacía al volumen de la celda ocupada por el sistema.
-    8. Convierte el volumen de angstrom cúbico a metros cúbicos.
+    The function performs the following steps:
+    1. Imports the specified XYZ file.
+    2. Modifies the cell vectors of the system to adjust the z coordinate value to 'lz'.
+    3. Applies transformations to the cell using the AffineTransformationModifier in OVITO.
+    4. Selects particles of type 'Ox' using the SelectTypeModifier.
+    5. Deletes the selected particles using the DeleteSelectedModifier.
+    6. Constructs a surface based on the AlphaShape method using the ConstructSurfaceModifier.
+    7. Calculates the free volume by subtracting the volume of the empty cell from the volume of the cell occupied by the system.
+    8. Converts the volume from cubic angstroms to cubic meters.
 
-    Ejemplo de uso:
-    input_file = "/ruta/al/archivo.xyz"
+    Example usage:
+    input_file = "/path/to/file.xyz"
     lz = 60.0
     free_volume, total_volume = calculate_free_volume(input_file, lz)
-    print(f"Volumen libre: {free_volume} m³")
+    print(f"Free volume: {free_volume} m³")
     """
 
-    # Importar archivo
+    # Import file
     pipeline = import_file(input_file)
     data = pipeline.compute()
 
     lx = data.cell[0, 0]
     ly = data.cell[1, 1]
 
-    # Modificar los vectores de celda
+    # Modify the cell vectors
     data.cell[2, 2] = lz
     data.cell.pbc = (True, True, True)
 
-    # Agregar el modificador AffineTransformationModifier para aplicar transformaciones absolutas a la celda
+    # Add the AffineTransformationModifier to apply absolute transformations to the cell
     pipeline.modifiers.append(AffineTransformationModifier(
         relative_mode=False,
         target_cell=pipeline.compute(0).cell[...]
     ))
 
-    # Aplicar el modificador SelectTypeModifier para seleccionar todas las partículas del tipo 'Ox'
+    # Apply the SelectTypeModifier to select all particles of type 'Ox'
     pipeline.modifiers.append(SelectTypeModifier(
         operate_on="particles",
         property="Particle Type",
         types={'Ox', 'Cgr'}
     ))
 
-    # Agregar el modificador AffineTransformationModifier para aplicar transformaciones absolutas a la celda nuevamente
+    # Add the AffineTransformationModifier to apply absolute transformations to the cell again
     pipeline.modifiers.append(AffineTransformationModifier(
         relative_mode=False,
         target_cell=data.cell[...]
     ))
 
-    # Agregar el modificador DeleteSelectedModifier para eliminar las partículas seleccionadas
+    # Add the DeleteSelectedModifier to delete the selected particles
     pipeline.modifiers.append(DeleteSelectedModifier())
 
-    # Agregar el modificador ConstructSurfaceModifier para construir una superficie basada en el método AlphaShape
+    # Add the ConstructSurfaceModifier to construct a surface based on the AlphaShape method
     pipeline.modifiers.append(ConstructSurfaceModifier(
         method=ConstructSurfaceModifier.Method.AlphaShape,
         radius=3.0,
@@ -74,10 +74,9 @@ def calculate_free_volume(input_file, lz):
 
     data = pipeline.compute()
 
-
     empty_volume = data.attributes['ConstructSurfaceMesh.empty_volume']
 
-    total_volume = (lx * ly * lz) * 1e-30  # Convertir de A^3 a m^3
-    free_volume = empty_volume * 1e-30  # Convertir de A^3 a m^3
+    total_volume = (lx * ly * lz) * 1e-30  # Convert from A^3 to m^3
+    free_volume = empty_volume * 1e-30  # Convert from A^3 to m^3
 
     return free_volume, total_volume
