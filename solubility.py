8b573745025844a512ae783feff029852db019bf
Juan Jiménez <98825720+jjimenezgar@users.noreply.github.com>
2023-06-17 16:52:11 -0300

    Update solubility.py

--- solubility.py
+++ solubility.py
@@ -4,33 +4,33 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import free_volume
 
-# Ruta al archivo XYZ
-file = "/ruta/al/archivo.xyz"
+# Path to the XYZ file
+file = "/path/to/file.xyz"
 
 def calculate_num_atoms(file):
     """
-    Calcula el número promedio de átomos de oxígeno en una región específica de una trayectoria.
+    Calculates the average number of oxygen atoms in a specific region of a trajectory.
 
-    Parámetros:
-    - file (str): Ruta al archivo XYZ que contiene la trayectoria.
+    Parameters:
+    - file (str): Path to the XYZ file containing the trajectory.
 
-    Retorna:
-    - num_oxygen_atoms (int): Número promedio de átomos de oxígeno en la región especificada.
+    Returns:
+    - num_oxygen_atoms (int): Average number of oxygen atoms in the specified region.
 
-    Requiere la biblioteca MDAnalysis: https://www.mdanalysis.org/
+    Requires the MDAnalysis library: https://www.mdanalysis.org/
 
-    La función realiza los siguientes pasos:
-    1. Carga la trayectoria y la topología.
-    2. Selecciona solo los átomos de oxígeno en la región específica.
-    3. Calcula el número de átomos de oxígeno en cada paso de la trayectoria.
-    4. Retorna el número promedio de átomos de oxígeno.
+    The function performs the following steps:
+    1. Load the trajectory and topology.
+    2. Select only the oxygen atoms in the specific region.
+    3. Calculate the number of oxygen atoms in each frame of the trajectory.
+    4. Return the average number of oxygen atoms.
     """
-    # Carga la trayectoria y la topología
+    # Load the trajectory and topology
     u = mda.Universe(file)
 
     num_oxygen_atoms = []
     for ts in u.trajectory:
-        # Selecciona solo los átomos de oxígeno en la región específica
+        # Select only the oxygen atoms in the specific region
         oxygen = u.select_atoms("prop z < 30 and name Ox")
         num_oxygen_atoms.append(len(oxygen))
 
@@ -38,15 +38,15 @@ def calculate_num_atoms(file):
 
 def calculate_n_moles(n_atoms):
     """
-    Calcula el número de moles a partir del número de átomos.
+    Calculates the number of moles from the number of atoms.
 
-    Parámetros:
-    - n_atoms (float): Número de átomos.
+    Parameters:
+    - n_atoms (float): Number of atoms.
 
-    Retorna:
-    - n_moles (float): Número de moles.
+    Returns:
+    - n_moles (float): Number of moles.
 
-    Utiliza la constante de Avogadro para realizar la conversión.
+    Uses Avogadro's constant for the conversion.
     """
     avogadro = 6.022e23
     n_moles = n_atoms / avogadro
@@ -54,64 +54,66 @@ def calculate_n_moles(n_atoms):
 
 def calculate_concentration(n_moles, volume):
     """
-    Calcula la concentración a partir del número de moles y el volumen.
+    Calculates the concentration from the number of moles and volume.
 
-    Parámetros:
-    - n_moles (float): Número de moles.
-    - volume (float): Volumen en metros cúbicos.
+    Parameters:
+    - n_moles (float): Number of moles.
+    - volume (float): Volume in cubic meters.
 
-    Retorna:
-    - concentration (float): Concentración en moles por metro cúbico.
+    Returns:
+    - concentration (float): Concentration in moles per cubic meter.
     """
     concentration = n_moles / volume
     return concentration
 
 def calculate_pressure(n_moles, volume):
     """
-    Calcula la presión a partir del número de moles y el volumen.
+    Calculates the pressure from the number of moles and volume.
 
-    Parámetros:
-    - n_moles (float): Número de moles.
-    - volume (float): Volumen en metros cúbicos.
+    Parameters:
+    - n_moles (float): Number of moles.
+    - volume (float): Volume in cubic meters.
 
-    Retorna:
-    - pressure (float): Presión en atmósferas.
+    Returns:
+    - pressure (float): Pressure in atmospheres.
     """
     R = 8.314
     temperature = 298.15  # Kelvin
-    pressure_pa = (3.98e-21 * R * temperature) / 1.35e-24  # nmol para 2400 partículas y el volumen es el de la caja menos la esfera
-    pressure_atm = pressure_pa / 101325.0  # Convertir Pascal a atmósferas
+    pressure_pa = (3.98e-21 * R * temperature) / 1.35e-24  # nmol for 2400 particles and the volume is the box minus the sphere
+    pressure_atm = pressure_pa / 101325.0  # Convert Pascal to atmospheres
     return pressure_atm
 
 def calculate_solubility(concentration, pressure):
     """
-    Calcula la solubilidad a partir de la concentración y la presión.
+    Calculates the solubility from the concentration and pressure.
 
-    Parámetros:
-    - concentration (float): Concentración en moles por metro cúbico.
-    - pressure (float): Presión en atmósferas.
+    Parameters:
+    - concentration (float): Concentration in moles per cubic meter.
+    - pressure (float): Pressure in atmospheres.
 
-    Retorna:
-    - solubility (float): Solubilidad en moles por metro cúbico por atmósfera.
+    Returns:
+    - solubility (float): Solubility in moles per cubic meter per atmosphere.
     """
     solubility = concentration / pressure
     return solubility
 
-# Calcular propiedades
-volume = free_volume.calculate_free_volume(file, 30)
-n_atoms = calculate_num_atoms(file)
-n_moles = calculate_n_moles(n_atoms)
-C = calculate_concentration(n_moles, volume[1])  # moles por metro cúbico
-P = calculate_pressure(n_moles, volume[1])  # atm
-S = calculate_solubility(C, P)  # moles por metro cúbico por atm
 
-# Imprimir el reporte
-print("Reporte de Propiedades")
-print("----------------------")
-print("Volumen libre: ", volume[0], " m³")
-print("Volumen total: ", volume[1], " m³")
-print("Número de átomos: ", n_atoms)
-print("Número de moles: ", n_moles, " mol")
-print("Concentración: ", C, " mol/m³")
-print("Presión: ", P, " atm")
-print("Solubilidad: ", S, " mol/(m³·atm)")
+if __name__ == "__main__":
+    # Calculate properties
+    volume = free_volume.calculate_free_volume(file, 30)
+    n_atoms = calculate_num_atoms(file)
+    n_moles = calculate_n_moles(n_atoms)
+    C = calculate_concentration(n_moles, volume[1])  # moles per cubic meter
+    P = calculate_pressure(n_moles, volume[1])  # atm
+    S = calculate_solubility(C, P)  # moles per cubic meter per atm
+
+    # Print the report
+    print("Properties Report")
+    print("-----------------")
+    print("Free Volume: ", volume[0], " m³")
+    print("Total Volume: ", volume[1], " m³")
+    print("Number of Atoms: ", n_atoms)
+    print("Number of Moles: ", n_moles, " mol")
+    print("Concentration: ", C, " mol/m³")
+    print("Pressure: ", P, " atm")
+    print("Solubility: ", S, " mol/(m³·atm)")
