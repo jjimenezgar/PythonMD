64773d5af2a56d4e03e7c910e6e00caf28455658
Juan Jiménez <98825720+jjimenezgar@users.noreply.github.com>
2023-06-17 16:39:49 -0300

    Update atomic_strain.py

--- atomic_strain.py
+++ atomic_strain.py
@@ -5,51 +5,52 @@ from ovito.vis import Viewport, OSPRayRenderer
 
 def calculate_atomic_strain(input_file):
     """
-    Calcula la deformación atómica de un sistema y guarda una imagen de la simulación.
+    Calculates the atomic strain of a system and saves an image of the simulation.
 
     Args:
-        input_file (str): Ruta del archivo XYZ de entrada.
+        input_file (str): Path to the input XYZ file.
 
     Returns:
         None
     """
-    # Importar archivo
+    # Import file
     pipeline = import_file(input_file)
     pipeline.add_to_scene()
     data = pipeline.compute()
 
-    # Seleccionar todas las partículas que no queremos estudiar
+    # Select all particles we don't want to study
     pipeline.modifiers.append(SelectTypeModifier(
         operate_on="particles",
         property="Particle Type",
         types={'Atoms to delete'}
     ))
-    # Eliminar las partículas seleccionadas
+    # Delete selected particles
     pipeline.modifiers.append(DeleteSelectedModifier())
 
-    # Calcular la deformación atómica
+    # Calculate atomic strain
     pipeline.modifiers.append(AtomicStrainModifier(
         cutoff=3.0
     ))
 
-    # Codificar el color según la deformación de corte
+    # Color code based on shear strain
     pipeline.modifiers.append(ColorCodingModifier(
         operate_on="particles",
         property='Shear Strain',
         gradient=ColorCodingModifier.Rainbow()
     ))
 
-    data.cell.vis.enabled = False  # Desactivar la visualización de la celda
+    data.cell.vis.enabled = False  # Disable cell visualization
 
-    # Renderizar y guardar una imagen de la simulación
+    # Render and save an image of the simulation
     vp = Viewport(type=Viewport.Type.Front)
     vp.zoom_all()
     vp.render_image(filename='simulation.png',
                     size=(1000, 1000),
                     renderer=OSPRayRenderer())
 
-    print("La simulación se ha guardado como 'simulation.png'.")
+    print("The simulation has been saved as 'simulation.png'.")
 
-# Ejemplo de uso
-input_file = "/path/to/input_file.xyz"
-calculate_atomic_strain(input_file)
+if __name__ == "__main__":
+    # Example usage
+    input_file = "/path/to/input_file.xyz"
+    calculate_atomic_strain(input_file)
