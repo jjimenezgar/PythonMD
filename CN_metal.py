8051040e3c87a02e2ee40f1129ac99faae238bd5
Juan Jiménez <98825720+jjimenezgar@users.noreply.github.com>
2023-06-20 12:05:34 -0300

    Update and rename CN_mteal.py to CN_metal.py

--- CN_metal.py
+++ CN_metal.py
@@ -1,15 +1,15 @@
+import sys
 import numpy as np
 from ovito.io import import_file, export_file
 from ovito.modifiers import CoordinationAnalysisModifier, SelectTypeModifier, DeleteSelectedModifier
 from ovito.vis import Viewport, OSPRayRenderer
 
-
-def perform_cluster_analysis(input_file, particle_types, cutoff=3.0):
+def perform_cluster_analysis(input_file, excluded_particles, cutoff=3.0):
     """
     Performs coordination analysis on a given input file for specific particle types.
 
     :param input_file: Path to the input file containing particle data.
-    :param particle_types: Set of particle types to include in the analysis.
+    :param excluded_particles: Set of particle types to exclude from the analysis.
     :param cutoff: Distance cutoff for coordination determination (default: 3.0).
     """
     # Import file
@@ -23,20 +23,18 @@ def perform_cluster_analysis(input_file, particle_types, cutoff=3.0):
     pipeline.modifiers.append(SelectTypeModifier(
         operate_on="particles",
         property="Particle Type",
-        types=particle_types
+        types=~excluded_particles
     ))
 
     # Apply DeleteSelectedModifier to remove selected particles
     pipeline.modifiers.append(DeleteSelectedModifier())
 
     # Apply coordinate analysis
-    pipeline.modifiers.append(CoordinationAnalysisModifier(
-        cutoff=cutoff))
+    pipeline.modifiers.append(CoordinationAnalysisModifier(cutoff=cutoff))
 
     # Export results of the coordinate analysis
-    export_file(pipeline, "/outputfile.xyz", "xyz",
-    columns = [ "Coordination","Position.X", "Position.Y", "Position.Z"])
-
+    export_file(pipeline, "/metal.xyz", "xyz",
+                columns=["Coordination", "Position.X", "Position.Y", "Position.Z"])
 
     # Disable cell visualization
     data.cell.vis.enabled = False
@@ -46,8 +44,12 @@ def perform_cluster_analysis(input_file, particle_types, cutoff=3.0):
     vp.zoom_all()
     vp.render_image(filename='simulation.png', size=(1000, 1000), renderer=OSPRayRenderer())
 
-
 if __name__ == "__main__":
-    input_file = "/home/jjimenez/Documentos/ApendiceA/cn.xyz"
-    particle_types = {'C Cgr O1 O2 O3 O4 S H1 H2 F'}  # Set of particle types to exclude in the analysis
-    perform_cluster_analysis(input_file, particle_types)
+    if len(sys.argv) < 3:
+        print("Usage: python script.py input_file excluded_particles")
+        sys.exit(1)
+
+    input_file = sys.argv[1]
+    excluded_particles = set(sys.argv[2].split())
+
+    perform_cluster_analysis(input_file, excluded_particles)
