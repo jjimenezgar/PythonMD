ed7e69ad91180efcdbde8993a20ce698ed259a6a
Juan Jiménez <98825720+jjimenezgar@users.noreply.github.com>
2023-06-29 12:17:49 -0300

    Update contact_tool2.py

--- Morse_Pt-H2/sticking_coefficient_method/contact_tool2.py
+++ Morse_Pt-H2/sticking_coefficient_method/contact_tool2.py
@@ -7,144 +7,142 @@ from statistics import mean
 import sys
 
 
-def atom_contact(file,atom1,atom2,r_cut):
-
+def atom_contact(file, atom1, atom2, r_cut):
     '''
-    Esta función calcula el número de atomos que estan en contacto con la superficie"
-    file: Archivo de entrada
-    atom1: Atomo de referencia
-    atom2: Atomo em contacto
-    r_cut: radio de corte
-
+    This function calculates the number of atoms in contact with the surface.
+    
+    Parameters:
+    file: Input file
+    atom1: Reference atom
+    atom2: Atom in contact
+    r_cut: Cutoff radius
+    
+    Returns:
+    The mean number of atoms in contact with the surface.
     '''
-
-    u=mda.Universe(file)
+    u = mda.Universe(file)
 
     term1 = u.select_atoms('name {name_atom1}'.format(name_atom1=atom1))
     term2 = u.select_atoms('name {name_atom2}'.format(name_atom2=atom2))
 
-    number_atom1= len(term1)
-    number_atom2=len(term2)
+    number_atom1 = len(term1)
+    number_atom2 = len(term2)
 
-
-    list_contact=[]
-    for ts in u.trajectory: ## lee cada frame
-        count=0  # Cuenta el número de atomos en contacto
-        for i in range(number_atom1): ## Empieza por comparar el primer atomo con el resto
-            list_d=[]
-            for x in range(number_atom2): ### Compara el atomo anterior con la seleccción 2
-                r=term2[x].position-term1[i].position  # Calcula la diferencia vectorial
-                d=numpy.linalg.norm(r) #Calcula la distancia
-                if d < float(r_cut): # si la distancia es menor a X
-                    list_d.append(d)  ## añade los atomos que cuenta los coloca en una lista
+    list_contact = []
+    for ts in u.trajectory:
+        count = 0
+        for i in range(number_atom1):
+            list_d = []
+            for x in range(number_atom2):
+                r = term2[x].position - term1[i].position
+                d = numpy.linalg.norm(r)
+                if d < float(r_cut):
+                    list_d.append(d)
         
+            if len(list_d) > 0 and min(list_d) < float(r_cut):
+                count += 1
         
+        list_contact.append(count)
 
-            if  len(list_d) > 0 and min(list_d) < float(r_cut):  ## toma la menor distancia de la lista de atomos que cuenta anteriormente 
-                    count += 1  ## Cuenta los atomos en contacto
-        
-        list_contact.append(count) ## Crea una lista con los atomos en contacto
+    return mean(list_contact)
 
-    return (mean(list_contact))
 
-def cm_contact(file,atom1,atom2,r_cut):
-
+def cm_contact(file, atom1, atom2, r_cut):
     '''
-    Esta función calcula el número de atomos que estan en contacto con la superficie"
-    file: Archivo de entrada
-    atom1: Atomo de referencia
-    atom2: Atomo em contacto
-    r_cut: radio de corte
-    '''
-
-    u=mda.Universe(file)
-
-    term1 = u.select_atoms('index {name_atom1}'.format(name_atom1=atom1)) # Seleccionar grupo de atomos para calcular centro de masa
-    term2 = u.select_atoms('name {name_atom2}'.format(name_atom2=atom2)) # Seleccionar superficie
+    This function calculates the number of atoms in contact with the surface.
     
-    # MDAnalysis asigna las masas de los atomos automaticamente si estan bien escritos los elementos
+    Parameters:
+    file: Input file
+    atom1: Reference atom
+    atom2: Atom in contact
+    r_cut: Cutoff radius
+    
+    Returns:
+    The contact probability based on the center of mass of atom1 with the surface.
+    '''
+    u = mda.Universe(file)
 
-    number_atom2=len(term2)
+    term1 = u.select_atoms('index {name_atom1}'.format(name_atom1=atom1))
+    term2 = u.select_atoms('name {name_atom2}'.format(name_atom2=atom2))
 
+    number_atom2 = len(term2)
 
-    list_contact=[]
-    for ts in u.trajectory: ## lee cada frame
-        count=0  # Cuenta el número de atomos en contacto
-        list_d=[]
-        cm = term1.center_of_mass() ## Centro de masa
-        for x in range(number_atom2): ### Compara el centro de masa con la superficie
-                r=cm-term2[x].position  # Calcula la diferencia vectorial
-                d=numpy.linalg.norm(r) #Calcula la distancia        
-                if d < float(r_cut): # si la distancia es menor a X
-                    list_d.append(d)  ## añade los atomos que cuenta los coloca en una lista
-        if  len(list_d) > 0 and min(list_d) < float(r_cut):  ## toma la menor distancia de la lista de atomos que cuenta anteriormente 
-            count += 1  ## Cuenta los atomos en contacto
-        list_contact.append(count) ## Crea una lista con los atomos en contacto
+    list_contact = []
+    for ts in u.trajectory:
+        count = 0
+        list_d = []
+        cm = term1.center_of_mass()
+        for x in range(number_atom2):
+            r = cm - term2[x].position
+            d = numpy.linalg.norm(r)
+            if d < float(r_cut):
+                list_d.append(d)
+        
+        if len(list_d) > 0 and min(list_d) < float(r_cut):
+            count += 1
+        
+        list_contact.append(count)
 
-    splits = np.array_split(list_contact,50)  ### variable seria el numero de veces que se repite el calculo
-
-    touch_count=0
+    splits = np.array_split(list_contact, 50)
+    touch_count = 0
     for array in splits:
         if sum(list(array)) > 10:
-            touch_count += 1  ###  Si la molécula esta cerca mas de 5 frames(0.12 ps), se considera en contacto
+            touch_count += 1
 
-    probability=round(touch_count/50, 4)  ## tomamos la probabilidada de cada trayectoria en contacto y la dividimos por el total
+    probability = round(touch_count / 50, 4)
 
     return probability
 
 
-def surface_contact(file,atom1,atom2,r_cut):
-
+def surface_contact(file, atom1, atom2, r_cut):
     '''
-    Esta función calcula el número de atomos que estan en contacto en la superficie"
-    file: Archivo de entrada
-    atom1: Atomo de referencia
-    atom2: Atomo em contacto
-    r_cut: radio de corte
-    '''
-
-    u=mda.Universe(file)
-
-    term1 = u.select_atoms('name {name_atom1}'.format(name_atom1=atom1)) # Seleccionar la superficie
-    term2 = u.select_atoms('name {name_atom2}'.format(name_atom2=atom2)) # Seleccionar grupo de atomos para calcular centro de masa
+    This function calculates the number of atoms in contact with the surface.
     
-    # MDAnalysis asigna las masas de los atomos automaticamente si estan bien escritos los elementos
+    Parameters:
+    file: Input file
+    atom1: Reference atom
+    atom2: Atom in contact
+    r_cut: Cutoff radius
+    
+    Returns:
+    The mean number of atoms in contact on the surface.
+    '''
+    u = mda.Universe(file)
 
-    number_atom1= len(term1)
-    number_atom2=len(term2)
+    term1 = u.select_atoms('name {name_atom1}'.format(name_atom1=atom1))
+    term2 = u.select_atoms('name {name_atom2}'.format(name_atom2=atom2))
 
-   
-    list_contact=[]
+    number_atom1 = len(term1)
+    number_atom2 = len(term2)
+
+    list_contact = []
     for ts in u.trajectory:
-        count=0
-        for x in range(number_atom1):  ### Cambia el atomo a evaluar
-            list_d=[]
+        count = 0
+        for x in range(number_atom1):
+            list_d = []
             for i in range(number_atom2):
-                r=term1[x].position-term2[i].position
-                d=numpy.linalg.norm(r)
+                r = term1[x].position - term2[i].position
+                d = numpy.linalg.norm(r)
                 if d < float(r_cut):
                     list_d.append(d)
         
-        
-
-            if len(list_d) > 0 and   min(list_d) < float(r_cut):
-                    count += 1
+            if len(list_d) > 0 and min(list_d) < float(r_cut):
+                count += 1
         
         list_contact.append(count)
 
-    return (mean(list_contact))
-
+    return mean(list_contact)
 
 
 if __name__ == '__main__':
-
     '''
-    Esta función calcula el número de atomos que estan en contacto en la superficie"
-    file: Archivo de entrada
-    atom1: Atomo de referencia
-    atom2: Atomo em contacto
-    r_cut: radio de corte
+    This function calculates the number of atoms in contact with the surface.
+    
+    Parameters:
+    file: Input file
+    atom1: Reference atom
+    atom2: Atom in contact
+    r_cut: Cutoff radius
     '''
-
-    r=(globals()[sys.argv[1]](sys.argv[2],sys.argv[3],sys.argv[4],sys.argv[5]))   ### abrir función desde la terminal
+    r = (globals()[sys.argv[1]](sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5]))
     print(r)
